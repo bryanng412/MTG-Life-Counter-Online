fc74e3a4b9fc3f4eb8e900c5fdcf875bdcf73a12
bryanng412 <bryanng412@gmail.com>
2020-05-13 23:14:31 -0700

    add prod endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 import PlayerList from './components/PlayerList'
 import Settings from './components/Settings'
 
+const ENDPOINT = process.env.NODE_ENV === 'production'
+  ? 'https://life-counter-server.herokuapp.com'
+  : 'http://localhost:8080'
+
 const App = () => (
   <ThemeProvider>
     <CSSReset />
-    <SocketIOProvider url="http://localhost:8080">
+    <SocketIOProvider url={ENDPOINT}>
       <PlayerList />
       <Settings />
     </SocketIOProvider>
